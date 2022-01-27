ff059e44da14b77fd241376af22201184c445e05
Radek Myška <radek.myska@idealine.cz>
2022-01-27 15:10:47 +0100

    fix

--- CandyBox_nodejs/nodeJs_api/utils/logUtils.js
+++ CandyBox_nodejs/nodeJs_api/utils/logUtils.js
@@ -3,9 +3,27 @@
 const { Utils } = require("./utils");
 const utils = new Utils();
 
+fs = require("fs");
+
 class LogUtils {
   static log(className, message) {
-    console.log(
+    let logMessage = this.getLogMessage(className, message);
+    console.log(logMessage);
+    this.logToFile(logMessage);
+  }
+
+  static logToFile(logMessage) {
+    const path = "candyLog.txt";
+
+    fs.open(path, "a", 666, function (e, id) {
+      fs.write(id, logMessage + "\n", null, "utf8", function () {
+        fs.close(id, function () {});
+      });
+    });
+  }
+
+  static getLogMessage(className, message) {
+    return (
       utils.getActualDate() + " location: " + className + " message: " + message
     );
   }
